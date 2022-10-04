aee2a78d0f6ffc87a6c0f6047e633ef0528447e7
Billy Cheng <chengpitchi@yahoo.com>
2022-10-04 16:06:51 +1030

    development work completed

---
+++ assets/js/script.js
@@ -0,0 +1,124 @@
+var currentDateEl = $('#currentDay'); 
+var currentDate = moment().format('dddd, MMMM Do YYYY'); 
+var timeBlockEl = $('.time-block'); 
+var messageEl = $('#message'); 
+
+currentDateEl.text(currentDate); 
+
+var eventList = new Array(); 
+
+function initPage() {
+    // get the saved events in local storage and render the timeblock
+    eventList = JSON.parse(localStorage.getItem("eventList"));
+
+    renderTimeBlocks(); 
+}
+
+function renderTimeBlocks() {
+    //reset the timeblock element
+    timeBlockEl.empty(); 
+
+    for(var i = 9; i <= 17; i++) {
+        var sectionEl = $('<section>')
+
+        // create hour column
+        sectionEl.addClass('row d-flex'); 
+        sectionEl.attr('data-hour', i); 
+        timeBlockEl.append(sectionEl); 
+
+        var hourEl = $('<div>'); 
+        hourEl.addClass('hour col-3 col-md-2 col-lg-1'); 
+        var hourTextEl = $('<p>'); 
+        hourTextEl.text(moment(i, 'HH').format("hA")); 
+        hourTextEl.attr('style', 'margin-top:40%'); 
+        hourEl.append(hourTextEl); 
+        sectionEl.append(hourEl); 
+
+        // create description column 
+        var descEl = $('<textarea>'); 
+
+        // populate the save event if any
+        if (eventList !== null) {
+            for (var j = 0; j < eventList.length; j++) {
+                if (eventList[j].date == moment().format('YYYYMMDD') && 
+                    eventList[j].hour == i) {
+                    descEl.text(eventList[j].desc); 
+                }
+            }
+        }
+
+        // set the background color based on the time 
+        if (moment().format('H') < i) {
+            descEl.addClass('description future col-6 col-md-8 col-lg-10'); 
+            descEl.attr('disabled', false); 
+            descEl.attr('style', 'color:blue'); 
+        } else if (moment().format('H') == i) {
+            descEl.addClass('description present col-6 col-md-8 col-lg-10');
+            descEl.attr('disabled', false);  
+        } else {
+            descEl.addClass('description past col-6 col-md-8 col-lg-10'); 
+            descEl.attr('disabled', true); 
+            descEl.attr('style', 'color:black'); 
+        }
+
+        sectionEl.append(descEl); 
+
+        // create the save button 
+        var saveEl = $('<button>'); 
+        saveEl.addClass('saveBtn col-3 col-md-2 col-lg-1'); 
+
+        var iconEl = $('<i>'); 
+        iconEl.addClass('fa fa-save'); 
+        iconEl.attr('style', 'color:white'); 
+        saveEl.append(iconEl); 
+        sectionEl.append(saveEl); 
+    }
+}
+
+function saveEvent(event){
+    var btnClicked = $(event.target);
+
+    console.log(btnClicked.parent('section')); 
+    var eventItem = {
+        hour: btnClicked.parent('section').data('hour'), 
+        desc: btnClicked.parent('section').children().eq(1).val(), 
+        date: moment().format('YYYYMMDD')
+    }
+
+    // validate the input 
+    if (eventItem.hour < moment().format('H')) {
+        alert('Cannot save past event.'); 
+        return; 
+    }
+
+    if (eventItem.desc === '') {
+        alert('No input on the event description.')
+        return; 
+    }
+
+    if (eventList === null) {
+        eventList = new Array(); 
+        eventList.push(eventItem);    
+    } else {
+        // if there is an existing event for the same time, remove the old one from local storage
+        for (var i = 0; i < eventList.length; i++) {
+            if (eventList[i].date == moment().format('YYYYMMDD') && 
+                eventList[i].hour == eventItem.hour) {
+                eventList.splice(i, 1); 
+                break; 
+            }
+        }
+
+        eventList.push(eventItem);
+    }
+
+    localStorage.setItem('eventList', JSON.stringify(eventList)); 
+    alert('Event Information is saved successfully.'); 
+
+    renderTimeBlocks(); 
+}
+
+// delegate the event listener to the save buttons from the time block element
+timeBlockEl.on('click', '.saveBtn', saveEvent);
+
+initPage(); 
